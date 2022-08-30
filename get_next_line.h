6b58698099bcc90d00890c45f007cf5e0f751eac
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-08-30 14:35:29 -0400

    skeleton for header

--- get_next_line.h
+++ get_next_line.h
@@ -10,3 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+//Function that outputs lines from a given file descriptor.
+char	*get_next_line(int fd);
+
+#endif
