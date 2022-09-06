84f6e3e6b304cd7486614d61ae2d971421803039
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-06 15:08:49 -0300

    working critical function 1/3

--- get_next_line.c
+++ get_next_line.c
@@ -17,19 +17,50 @@
 //Function joins the initial string with reads from a descriptor until newline.
 char	*joining(int fd, char *initial)
 {
+	char	*read;
+	char	*out;
 
+	read = ft_fetch(fd, BUFFER_SIZE);
+	if (!read)
+		return ((char *) 0);
+	out = ft_strjoin(initial, read);
+	while (ft_strsrch(read, '\n') == 0 && read[0] != '\0')
+	{
+		read = ft_fetch(fd, BUFFER_SIZE);
+		out = ft_strjoin(out, read);
+	}
+	free(read);
+	return (out);
 }
+
+/* Test
+//gcc -Wall -Werror -Wextra get_next_line.c get_next_line_utils.c && ./a.out
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main()
+{
+	char	*ptr = "";
+	int		fd;
+
+	fd = open("sample.txt", O_RDONLY, 0);
+	ptr = joining(fd, ptr);
+	printf("Joined: %s", ptr);
+	free(ptr);
+	return (0);
+}
+//*/
 
 //Function trims the input string such that contents after newline are cut.
 char	*output(char *candidate)
 {
-
+	return ("");
 }
 
 //Function takes input string and returns contents after the first newline.
 char	*remaining(char *candidate)
 {
-
+	return ("");
 }
 
 char	*get_next_line(int fd)
