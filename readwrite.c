1c270e4cc11c68ef81cd8f5b8e808d55d792b641
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-06 12:28:17 -0300

    corrected function name call

--- readwrite.c
+++ readwrite.c
@@ -12,7 +12,7 @@ char	*ft_fetch(int fd)
 	if (!string)
 		return ((char *) 0);
 	rdout = read(fd, string, 4);
-	if (rdout == -1)
+	if (rdout == -1 || string[0] == '\0')
 	{
 		free(string);
 		return ((char *) 0);
@@ -26,11 +26,41 @@ int	main(void)
 	char	*ptr;
 
 	fd = open("sample.txt", O_RDONLY, 0);
-	ptr = stringy(fd);
-	printf("Read1: %s\n", ptr);
+	ptr = ft_fetch(fd);
+	printf("Read1:  %s\n", ptr);
 	free(ptr);
-	ptr = stringy(fd);
-	printf("Read2: %s\n", ptr);
+	ptr = ft_fetch(fd);
+	printf("Read2:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read3:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read4:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read5:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read6:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read7:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read8:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read9:  %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read10: %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read11: %s\n", ptr);
+	free(ptr);
+	ptr = ft_fetch(fd);
+	printf("Read12: %s\n", ptr);
 	free(ptr);
 	return (0);
 }
