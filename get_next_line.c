59877726f0604871b449e12c1b589d1103fa5f4d
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-06 14:31:43 -0300

    Initial outline of function

--- get_next_line.c
+++ get_next_line.c
@@ -10,3 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <limits.h>
+#include "get_next_line.h"
+
+//Function joins the initial string with reads from a descriptor until newline.
+char	*joining(int fd, char *initial)
+{
+
+}
+
+//Function trims the input string such that contents after newline are cut.
+char	*output()
+{
+
+}
+
+//Function takes input string and returns contents after the first newline.
+char	*remaining()
+{
+
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*ptr = "";
+	char		*out;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= INT_MAX)
+		return ((char *) 0);
+	ptr = joining(fd, ptr);
+	if (!ptr)
+		return ((char *) 0);
+	out = output(ptr);
+	ptr = remaining(ptr);
+	return (out);
+}
+
+/*Test
+//gcc get_next_line.c get_next_line_utils.c && ./a.out | cat -e
+
+int main(void)
+{
+	int		fd;
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	fd = open("sample.txt", O_RDONLY, 0);
+	while (ptr != (char *) 0)
+	{
+		ptr = get_next_line(fd);
+		printf("Read%d: %s", i, ptr);
+		free(ptr);
+		i++;
+	}
+	return (0);
+}
+//*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,3 +10,113 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+//Function determines length of the string (number of characters).
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/* Test | gcc -Wall -Werror -Wextra get_next_line_utils.c && ./a.out
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char *f = "Hello World";
+	char *d = "";
+	printf("Test 1: %s, strlen: %lu,
+				ft_strlen: %lu\n", f, strlen(f), ft_strlen(f));
+	printf("Test 2: %s, strlen: %lu,
+				ft_strlen: %lu\n", d, strlen(d), ft_strlen(d));
+	return (0);
+}
+//*/
+
+//Function joins two character strings, creates a new char string.
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned char	*ptr;
+	unsigned int	i;
+	unsigned int	j;
+
+	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!ptr)
+		return ((char *) 0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = 0;
+	return ((char *) ptr);
+}
+
+/* Test | gcc -Wall -Werror -Wextra get_next_line_utils.c && ./a.out
+#include <stdio.h>
+
+int	main(void)
+{
+	char	s1[] = "You";
+	char	s2[] = " and me";
+	char	*join;
+
+	join = ft_strjoin(s1, s2);
+	printf("Test: s1-%s, s2-%s, Out-%s", s1, s2, join);
+	free(join);
+	return (0);
+}
+//*/
+
+//Function reads from a file descriptor, creating a string for its output.
+char	*ft_fetch(int fd, int buffsize)
+{
+	char	*string;
+	int		rdout;
+
+	string = malloc ((buffsize + 1) * sizeof(char));
+	if (!string)
+		return ((char *) 0);
+	rdout = read(fd, string, buffsize);
+	if (rdout == -1 || string[0] == '\0')
+	{
+		free(string);
+		return ((char *) 0);
+	}
+	return (string);
+}
+
+//* Test | gcc -Wall -Werror -Wextra get_next_line_utils.c && ./a.out
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*fetched;
+	int		fd;
+	int		buffsize;
+
+	fd = open("sample.txt", O_RDONLY, 0);
+	buffsize = 6;
+	fetched = ft_fetch(fd, buffsize);
+	printf("Read: %s", fetched);
+	free(fetched);
+	return (0);
+}
+//*/
