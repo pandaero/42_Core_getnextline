68a5959459b73161248c253872c157ae106355ab
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-11 13:53:36 -0300

    Made the printing inside a loop

--- readwrite.c
+++ readwrite.c
@@ -8,10 +8,10 @@ char	*ft_fetch(int fd)
 	char	*string;
 	int		rdout;
 
-	string = malloc ((4 + 1) * sizeof(char));
+	string = malloc ((5 + 1) * sizeof(char));
 	if (!string)
 		return ((char *) 0);
-	rdout = read(fd, string, 4);
+	rdout = read(fd, string, 5);
 	if (rdout == -1 || string[0] == '\0')
 	{
 		free(string);
@@ -24,45 +24,20 @@ int	main(void)
 {
 	int		fd;
 	char	*ptr;
+	int		i;
 
 	fd = open("sample.txt", O_RDONLY, 0);
-	ptr = ft_fetch(fd);
-	printf("Read1:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read2:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read3:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read4:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read5:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read6:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read7:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read8:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read9:  %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read10: %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read11: %s\n", ptr);
-	free(ptr);
-	ptr = ft_fetch(fd);
-	printf("Read12: %s\n", ptr);
-	free(ptr);
+	i = 0;
+	while (i < 17)
+	{
+		ptr = ft_fetch(fd);
+		if (i < 10)
+			printf("Read  %d: %s\n", i, ptr);
+		else
+			printf("Read %d: %s\n", i, ptr);
+		free(ptr);
+		i++;
+	}
 	return (0);
 }
-
 //gcc -Wall -Werror -Wextra readwrite.c && ./a.out | cat -e
