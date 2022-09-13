0ebc60a83512c6213809149bd919ce8ee2977e4f
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-13 16:25:43 -0300

    changed variable name for clarity

--- gnl_main.c
+++ gnl_main.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int		fd;
 	int		i;
-	char	*ptr;
+	char	*line;
 
 	//Standard Input, close it with CTRL + D
 	fd = 0;
 	fd = open("sample.txt", O_RDONLY, 0);
 	i = 1;
-	while (ptr != (char *) 0)
+	while (line != (char *) 0)
 	{
-		ptr = get_next_line(fd);
-		printf("Line%d: %s", i, ptr);
-		free(ptr);
+		line = get_next_line(fd);
+		printf("Line%d: %s", i, line);
+		free(line);
 		i++;
 	}
 	return (0);
