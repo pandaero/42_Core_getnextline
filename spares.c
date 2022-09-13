94081245344c9370870c224127dd2edfb213c980
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-13 12:01:57 -0300

    added fetch to spares

--- spares.c
+++ spares.c
@@ -33,6 +33,52 @@ int	main(void)
 }
 //*/
 
+//Function reads from a file descriptor, creating a string for its output.
+char	*ft_fetch(int fd, int buffsize)
+{
+	char	*string;
+	int		rdout;
+
+	rdout = 0;
+	string = malloc ((buffsize + 1) * sizeof(char));
+	if (!string)
+		return ((char *) 0);
+	string[0] = '\0';
+	rdout = read(fd, string, buffsize);
+	if (rdout == -1 || string[0] == '\0')
+	{
+		free(string);
+		return ((char *) 0);
+	}
+	string[buffsize] = '\0';
+	return (string);
+}
+
+/* Test | gcc -Wall -Werror -Wextra get_next_line_utils.c && ./a.out
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*fetched;
+	int		fd;
+	int		buffsize;
+	int		i;
+
+	fd = open("sample.txt", O_RDONLY, 0);
+	buffsize = 6;
+	i = 1;
+	while (i < 20)
+	{
+		fetched = ft_fetch(fd, buffsize);
+		printf("Read%d: %s\n", i, fetched);
+		free(fetched);
+		i++;
+	}
+	return (0);
+}
+//*/
+
 //Function counts on a static variable array. Input determines counter mode.
 int	*ft_count(int ctr)
 {
