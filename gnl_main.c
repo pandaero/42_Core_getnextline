1d699222681584b246b8f02607ecc39189c8e11e
pandalaf <pandalaf@student.42.wolfsburg.de>
2022-09-13 16:18:55 -0300

    style and added library

--- gnl_main.c
+++ gnl_main.c
@@ -2,6 +2,7 @@
 //gcc gnl_main.c get_next_line.c get_next_line_utils.c && ./a.out | cat -e
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "get_next_line.h"
 
 int main(void)
@@ -10,10 +11,10 @@ int main(void)
 	int		i;
 	char	*ptr;
 
-	i = 0;
 	//Standard Input, close it with CTRL + D
 	fd = 0;
 	fd = open("sample.txt", O_RDONLY, 0);
+	i = 1;
 	while (ptr != (char *) 0)
 	{
 		ptr = get_next_line(fd);
